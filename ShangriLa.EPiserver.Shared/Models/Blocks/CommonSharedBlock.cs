1056bc79fc3e7d4613d94bb0c042f6083a90c8c8
Arthur.Mak <Arthur.Mak@SLIM27MKT127H6W>
2016-07-06 16:26:01 +0800

    Add Project ShangriLa.EPiServer.Shared

---
+++ ShangriLa.EPiserver.Shared/Models/Blocks/CommonSharedBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+
+
+namespace ShangriLa.EPiServer.Shared.Models.Blocks
+{
+    [ContentType(DisplayName = "CommonBlock Shared", GUID = "FA6DB103-1289-458A-9D66-55BA77E843B6", Description = "")]
+    public class CommonSharedBlock : BlockData
+    {
+                [CultureSpecific]
+                [Display(
+                    Name = "CommonSharedText",
+                    Description = "CommonSharedText",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual string CommonSharedText { get; set; }
+         
+    }
+}
